c27f8670951795e9efd6db06faffd26ee62ebd23
Heliostat9 <heliostat@inbox.ru>
2021-05-01 20:17:23 +0300

    'init'

---
+++ src/components/app-header/app-header.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+import './app-header.css'
+import styled from 'styled-components'
+
+const Header = styled.div`
+    display: flex;
+    align-tems: flex-end;
+    justify-content: space-between;
+    h1 {
+        font-size: 26px;
+        color: ${props => props.colored ? 'red' : 'black'};
+        :hover {
+            color: blue;
+        }
+    }
+
+    h2 {
+        font-size: 1.2rem;
+        color: grey;
+    }
+    
+`
+
+const AppHeader = ({liked, allPosts}) => {
+    return (
+        <Header>
+            <h1>Artyom Hitrin</h1>
+            <h2>{allPosts} записей, из них понравилось {liked}</h2>
+        </Header>
+    )
+}
+
+export default AppHeader;
